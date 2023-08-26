3d135a4b4ae669aba6e992aa86b9de3cc836bfd2
Decision <personal@decision01.com>
2023-08-26 23:27:03 +0800

    Initial Commit

---
+++ Artemis Editor/Artemis_EditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Artemis_EditorApp.swift
+//  Artemis Editor
+//
+//  Created by 熊晨曦 on 2023/8/5.
+//
+
+import SwiftUI
+
+@main
+struct Artemis_EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
